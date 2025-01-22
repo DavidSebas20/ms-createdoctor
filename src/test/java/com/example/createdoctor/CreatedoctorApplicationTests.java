b2868cfe9e87c29895b0f3aa495003d97a0db1fd
DavidSebas20 <davidsebastian20@outlook.com>
2025-01-21 23:12:24 -0500

    :tada: initialize project structure with Spring Boot and Maven setup

---
+++ src/test/java/com/example/createdoctor/CreatedoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.createdoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatedoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
