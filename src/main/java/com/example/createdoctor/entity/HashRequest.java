1a242fd6446f3bbee8ebc5f292a776f7725e42ab
DavidSebas20 <davidsebastian20@outlook.com>
2025-01-23 13:25:45 -0500

    feat(entity): add HashRequest and HashResponse entities for password handling

---
+++ src/main/java/com/example/createdoctor/entity/HashRequest.java
@@ -0,0 +1,18 @@
+package com.example.createdoctor.entity;
+
+public class HashRequest {
+    private String password;
+
+    public HashRequest(String password) {
+        this.password = password;
+    }
+
+    // Getters y Setters
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/example/createdoctor/entity/HashResponse.java
@@ -0,0 +1,10 @@
+package com.example.createdoctor.entity;
+
+
+public class HashResponse {
+    private String hash;
+
+    // Getters y Setters
+    public String getHash() { return hash; }
+    public void setHash(String hash) { this.hash = hash; }
+}
